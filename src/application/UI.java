29919b0661d6b50873b785b9de9b3c28f9a01118
samuelevi87 <samuel.levi.alves@outlook.com>
2020-09-12 04:23:23 -0300

    Moving Pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
 
-    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
@@ -26,25 +30,34 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
+		}
+	}
+	
+	public static void printBoard(ChessPiece[][] pieces) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
 
-
-
-
-    public static void printBoard(ChessPiece[][] pieces) {
-        for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
-            for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println("  a b c d e f g h");
-    }
-
-    private static void printPiece(ChessPiece piece) {
-        if (piece == null) {
+	private static void printPiece(ChessPiece piece) {
+    	if (piece == null) {
             System.out.print("-");
-        } else {
+        }
+        else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
             }
@@ -53,5 +66,5 @@ public class UI {
             }
         }
         System.out.print(" ");
-    }
+	}
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,21 +1,16 @@
 package boardgame;
 
 public class Piece {
-    protected Position position;
-    private Board board;
 
-    public Piece(Board board) {
-        this.board = board;
-        position = null;
-    }
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
 
-    protected Board getBoard() { // este tabuleiro é de uso interno desta camada, por isso está protegido.
-        return board;
-    }
-
-
-
-    
-
-    
+	protected Board getBoard() {
+		return board;
+	}
 }
